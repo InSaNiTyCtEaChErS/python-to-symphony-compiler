e63d3379be3eb5cc46105f2626eff5b423c88749
InSaNiTyCtEaTuReS <void7535@gmail.com>
2025-10-28 11:27:28 -0500

    Update main compiler

--- python_to_symphony_compiler.py
+++ python_to_symphony_compiler.py
@@ -2,5 +2,19 @@ with open("input.txt","r") as file:
     code = file.read()
     print(code)
 
+with open("basic functions.txt","r") as file:
+    compiled = file.readlines
+    print(compiled)
+variables = []
+
 def print_(str_):
-    print("print:"+str_)
+    i = 0
+    char_ = ""
+    if not str_ in variables:
+        for char in str_:
+            i += 1
+            if i%2==0:
+                compiled.append("<U32>0b0010010000010000"+str(bin(ord(char)))+str(bin(ord(char_))))
+            else:
+                char_ = char
+
