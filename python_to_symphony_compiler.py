3afd626e71a8cb1164ad39dba3c6038743e40212
InSaNiTyCtEaTuReS <void7535@gmail.com>
2025-10-31 12:02:28 -0500

    finish up printing strings with tests

--- python_to_symphony_compiler.py
+++ python_to_symphony_compiler.py
@@ -3,18 +3,35 @@ with open("input.txt","r") as file:
     print(code)
 
 with open("basic functions.txt","r") as file:
-    compiled = file.readlines
-    print(compiled)
-variables = []
+    end_file = file.read()
+    print(end_file)
+
+compiled = []
+variables = ["x"]
+
+def c_a(a):
+    compiled.append(a)
 
 def print_(str_):
     i = 0
     char_ = ""
+    if type(str_) == int and (str_ >=32768 or str_ <= -32769):
+        print(">>> Keyerror: int out of bounds")
+        return
     if not str_ in variables:
         for char in str_:
             i += 1
             if i%2==0:
-                compiled.append("<U32>0b0010010000010000"+str(bin(ord(char)))+str(bin(ord(char_))))
+                c_a("push r1")
+                c_a("<U32>0b0010010000010000"+str(bin(ord(char)))+str(bin(ord(char_))))
+                c_a("call printstr")
+                c_a("pop r1")
             else:
                 char_ = char
+    else: 
+        c_a("push r1")
+        c_a("load_16 ["+str(variables.index(str_))+"],r1")
+        c_a("call printint")
+        c_a("pop r1")
 
+print_("x")
